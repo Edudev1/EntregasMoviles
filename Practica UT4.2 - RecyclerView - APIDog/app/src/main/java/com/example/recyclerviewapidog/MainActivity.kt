712f04cccbe66a9158555f74472ba5abacb6a226
edudev1 <anguloeduardo001@gmail.com>
2026-03-09 19:50:46 +0100

    4

--- Practica UT4.2 - RecyclerView - APIDog/app/src/main/java/com/example/recyclerviewapidog/MainActivity.kt
+++ Practica UT4.2 - RecyclerView - APIDog/app/src/main/java/com/example/recyclerviewapidog/MainActivity.kt
@@ -7,20 +7,15 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerviewapidog.databinding.ActivityMainBinding
-import com.example.recyclerviewapidog.model.MainState
 import com.example.recyclerviewapidog.recycler.MiAdaptador
 import com.example.recyclerviewapidog.viewmodel.RecyclerViewModel
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val miViewModel : RecyclerViewModel by viewModels()
+    private val miViewModel: RecyclerViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,13 +24,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         enableEdgeToEdge()
 
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         binding.rvPerros.layoutManager = LinearLayoutManager(this)
+
         binding.btnBuscar.setOnClickListener {
             val raza = binding.etBuscar.text.toString().trim().lowercase()
 
@@ -46,12 +42,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        miViewModel.datos.observe(this) {
-            respuesta -> if (respuesta.status == "success" && !respuesta.message.isNullOrEmpty()) {
-                        } else {
-                            Toast.makeText(this, "No se encontraron fotos de perros de esa raza",
-                                Toast.LENGTH_SHORT).show()
-        }
+        miViewModel.datos.observe(this) { respuesta ->
+            if (respuesta.status == "success" && !respuesta.message.isNullOrEmpty()) {
+                binding.rvPerros.adapter = MiAdaptador(respuesta)
+            } else {
+                Toast.makeText(this, "No se encontraron fotos de perros de esa raza",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- Practica UT4.2 - RecyclerView - APIDog/app/src/main/java/com/example/recyclerviewapidog/model/DogRespuesta.kt
+++ Practica UT4.2 - RecyclerView - APIDog/app/src/main/java/com/example/recyclerviewapidog/model/DogRespuesta.kt
@@ -1,7 +1,4 @@
 package com.example.recyclerviewapidog.model
 
-import android.hardware.camera2.CameraExtensionSession
-import android.os.Message
-
 class DogRespuesta(val status: String, val message: List<String>?) {
 }
